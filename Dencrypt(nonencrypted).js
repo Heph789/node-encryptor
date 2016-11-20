a870b3cfb846be02ee649c4de5ae9097a1a1bd4e
Heph789 <chaselbjeter@gmail.com>
2016-11-19 19:32:38 -0500

    Add files via upload

---
+++ Dencrypt(nonencrypted).js
@@ -0,0 +1,40 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const key = 'Poseidon789';
+const cipher = crypto.createCipher('aes192', key);
+const decipher = crypto.createDecipher('aes192', key);
+exports.encrypt = function (where, pass) {
+  var encrypted = cipher.update(pass, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  var datset = {
+    place: where,
+    passwd: encrypted
+  };
+  var datfull = [];
+  fs.readFile('/Users/angiebarrington-jeter/Desktop/Code/JS/SP/stor.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    datfull = JSON.parse(data);
+    datfull.push(datset);
+    fs.writeFile('/Users/angiebarrington-jeter/Desktop/Code/JS/SP/stor.json', JSON.stringify(datfull), function (err) {
+      if(err) throw err;
+      console.log('Completed');
+    });
+  });
+};
+
+exports.decrypt = function (where) {
+  var encryptedDat = '';
+  fs.readFile('/Users/angiebarrington-jeter/Desktop/Code/JS/SP/stor.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    var parsedDat = JSON.parse(data);
+    for (i=0; i < parsedDat.length; i++) {
+      if (parsedDat[i].place == where) {
+        encryptedDat += parsedDat[i].passwd;
+        break;
+      }
+    }
+    var decrypted = decipher.update(encryptedDat, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    console.log('Your password is: ' + decrypted);
+  });
+};
---
+++ main(nonencrypted).js
@@ -0,0 +1,46 @@
+//Loads the readline module in
+const readline = require('readline');
+const denc = require('./Dencrypt.js');
+
+//Creats the readline interface for receiving and prompting information
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+//asks for the password
+rl.question('What is the password? ', function checkPass(pass) {
+  //if the password is correct
+  if (pass == 'Poseidon789') {
+    //asks another question
+    rl.question('Type "r" if you are retrieving password and "a" if you are adding password: ', function (rora) {
+      main(rora.toUpperCase());
+    });
+  }
+  //throws an error and kills the program if the password is wrong
+  else {
+    console.log('The password is wrong');
+    rl.close();
+  }
+});
+function main(proc) {
+  //if they want to retrieve a password
+  if (proc == 'R') {
+    rl.question('What is your password for? ', function (where) {
+      denc.decrypt(where);
+      rl.close();
+    });
+  }
+  //if they want to add a password
+  else if (proc == 'A') {
+    rl.question('What is your password for? ', function (where) {
+      rl.question('What is your password? ', function (pass2) {
+        denc.encrypt(where, pass2);
+        rl.close();
+      });
+    });
+  }
+  else {
+    console.log('Not valid');
+    rl.close();
+  }
+}
